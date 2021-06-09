89f75118be7458ba751719b8f73d7e469987476d
marc459 <santosserranomarcos@gmail.com>
2021-06-09 18:42:01 +0200

    gnlsimplificated normi and refactor

---
+++ gnl_simplificated/get_next_line60.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line60.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 14:35:59 by marcos            #+#    #+#             */
+/*   Updated: 2021/06/09 18:18:19 by msantos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line60.h"
+
+char	*ft_strjoint(char *s, char c)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	dest = malloc(ft_strlen(s) + 2);
+	while (s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = c;
+	dest[i + 1] = '\0';
+	free(s);
+	return (dest);
+}
+
+int	get_next_line(char **line)
+{
+	int		ret;
+	char	buf[2];
+
+	*line = ft_strdup("");
+	if (line == NULL || !line)
+		return (-1);
+	ret = read(0, buf, 1);
+	while (ret > 0)
+	{
+		if (buf[0] == '\n')
+			break ;
+		*line = ft_strjoint(*line, buf[0]);
+	}
+	return (ret);
+}
--- gnl_simplificated/get_next_line60.h
+++ gnl_simplificated/get_next_line60.h
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line60.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/12 19:38:34 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/11/04 14:09:33 by msantos-         ###   ########.fr       */
+/*   Created: 2021/06/09 18:12:08 by msantos-          #+#    #+#             */
+/*   Updated: 2021/06/09 18:12:53 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#	ifndef GET_NEXT_LINE60_H
+# define GET_NEXT_LINE60_H
 
-# include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 # include <fcntl.h>
+# define BUFFER_SIZE 1
 
-# define BUFFER_SIZE 10
-
-int		get_next_line(int fd, char **line);
+int		get_next_line(char **line);
+char	*ft_strjoint(char *s, char c);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *str, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
